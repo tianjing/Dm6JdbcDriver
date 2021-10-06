938eed4cc6cba6d3b20c32b82ebd26f24af08aa9
tianjing <tian___jing@hotmail.com>
2021-10-06 11:10:51 +0800

    第1个版本 初始化项目

---
+++ lib/install.bat
@@ -0,0 +1,2 @@
+set JAVA_HOME=D:/public/java/zulu8.33.0.1-jdk8.0.192-win_x64
+C:/Work/apache-maven-3.6.0/bin/mvn install:install-file -DgroupId=dm -DartifactId=dm.Dm6JdbcDriver -Dversion=1.0.0 -Dfile=C:\Work\apache-maven-3.6.0\install\Dm6JdbcDriver\dm.Dm6JdbcDriver-1.0.0.jar -Dpackaging=jar -DgeneratePom=true
